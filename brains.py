a062c654b751815bb5796828bec6c51939aa883f
Eric Neuman <eric.neuman@citizengroove.com>
2011-06-26 00:02:07 -0400

    added the first ever file

---
+++ brains.py
@@ -0,0 +1,38 @@
+class Problem(object):
+    data = {}
+    model = None
+    
+    def __init__(self, modelstate):
+        "heres the model and we don't know anything about it.  That's the problem"
+        
+        
+    def validate(self, modelstate):
+        "evaluates the state and returns some numerical score"
+        pass
+
+class ModelState(object):
+    "an instance of this is one state of the model, also known as a solution"
+    
+class GraphTraverser(representation)
+    "an object that can be moved around on a graph (like a monkey, or a chess piece)"
+    
+class Graph(representation):
+    "some mathematical graph object we'll probably steal"
+    
+class OperationList(representation):
+    "some mathematical graph object we'll probably steal"
+    
+class Constraint(object):
+    "defines a rule that must be met within the ModelState"
+    #types: 
+        #graph (
+
+
+class Algorithm:
+    def cyle(self):
+        "returns a new state for validation by the problem"
+
+class Explorer(Algorithm):
+
+    def cycle(self):
+    pass 
