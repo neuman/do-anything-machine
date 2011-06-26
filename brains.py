654952129c5bacb090f779b30fb33f626e219ded
Eric Neuman <eric.neuman@citizengroove.com>
2011-06-26 13:11:15 -0400

    more development in brains

--- brains.py
+++ brains.py
@@ -1,23 +1,55 @@
+from pygraph.classes.graph import graph
+
 class Problem(object):
     data = {}
     model = None
     
     def __init__(self, modelstate):
         "heres the model and we don't know anything about it.  That's the problem"
-        
+        `
         
     def validate(self, modelstate):
         "evaluates the state and returns some numerical score"
         pass
 
-class ModelState(object):
+class Model(object):
     "an instance of this is one state of the model, also known as a solution"
+    
+    def get_genes(self):
+        "returns an array of integers"
+        return []
+        
+    def get_geneSpace(self):
+        "returns an array of arrays where each column represents one gene and all of its possible values"
+        return [[]]
+
+    
+class Graph(representation):
+    "some mathematical graph object we'll probably steal"
+    graph = None
+    traversers = []
     
 class GraphTraverser(representation)
     "an object that can be moved around on a graph (like a monkey, or a chess piece)"
     
-class Graph(representation):
-    "some mathematical graph object we'll probably steal"
+    name = None
+    #location is a node and a graphh
+    node = None
+    graph = None
+    
+    def set_location(self, node, graph):
+        #remove self from other graph 
+        self.node = node
+        self.graph = graph
+        #if graph.traversers doesn't contain self, add self
+        if not self.graph.traversers.__contains__(self):
+            self.graph.traversers.append(self)
+        
+    
+    
+    def __init__(self):
+        #make the graph variable an actual object
+        graph = graph()
     
 class OperationList(representation):
     "some mathematical graph object we'll probably steal"
@@ -29,10 +61,12 @@ class Constraint(object):
 
 
 class Algorithm:
+    problem = None
+    
+    def __init__(self, problem):
+        self.problem = problem
+    
     def cyle(self):
         "returns a new state for validation by the problem"
+        return None
 
-class Explorer(Algorithm):
-
-    def cycle(self):
-    pass 
