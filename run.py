5daf7c01305c243c13c6cc58a5d1ae23972e5c7f
Eric Neuman <soloptimus@gmail.com>
2013-05-31 00:38:56 -0400

    please run run.py to see (gasp) a very simple but working loop involving a question class, 2 seekers and 1 found solution (two ways).

--- run.py
+++ run.py
@@ -1,23 +1,42 @@
-import models.model as models
-import algorithms.supervised as supervised
-t = models.Test()
-space = t.space
-current = t.space.current()
+from models import base as models
+from seekers.Exhaustive import Exhaustive
+from seekers.DiceRolling import DiceRolling
+from questions.WhatsMyNumber import WhatsMyNumber
 
-#q = models.NineQueens()
-#print q.space
-#print q.space.current()
+def run(question, seeker, verbose=False):
+    seeker = seeker(question.model)
+    go = True
+    tries = 0
+    while go==True:
+        try:
+            tries +=1
+            current = seeker.next()
+            if verbose:
+                print "checking"+str(current)
+            checked = question.check(current)
+            if checked.warmth == 1:
+                raise Exception("Answer to '"+str(question)+"' found in "+str(tries)+" tries using "+str(seeker)+":"+str(current))
 
-e = supervised.Exhaustive(t)
+        except Exception as e:
+            print e
+            go = False
 
 
-go = True
-output = []
-while go==True:
-    try:
-        print e.next()
-    except Exception as e:
-        print e
-        go = False
+q = WhatsMyNumber()
+print q.model.space.permutations
+run(q,Exhaustive)
+run(q,DiceRolling)
 
 
+
+sp = q.model.space
+n = sp.current
+
+
+
+
+
+
+'''
+Django for Machine Learning That Learns Itself
+'''
